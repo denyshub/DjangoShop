afdd7fd6c50da9aaf9ba0a5fd1e1e4cd90903c66
denyshub <denys.hubenko.ik.2022@lpnu.ua>
2024-07-08 13:34:32 +0300

    Start of goods development

--- application/main/views.py
+++ application/main/views.py
@@ -5,10 +5,16 @@ from django.shortcuts import render
 def index(request):
   context: dict[str, str] = {
     'title': 'Home',
-    "content" : 'Головна сторінка магазину - HOME'
+    'content' : 'Головна сторінка магазину - HOME',
   }
 
   return render(request, 'main/index.html', context)
 
 def about(request):
-  return HttpResponse('About page')
+  context: dict[str, str] = {
+    'title': 'About',
+    'content' : 'Сторінка про магазин',
+    'text_on_page': 'Текст про те чому ви маєте обрати саме наш магазин.'
+  }
+
+  return render(request, 'main/about.html', context)
